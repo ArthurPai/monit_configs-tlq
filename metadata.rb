9434eb546c094c95bcef3d6161e33af9ca9a0dd9
ArthurPai <baidragoon@gmail.com>
2015-02-08 18:04:46 +0800

    update systemctl path

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "monit_configs-tlq"
 maintainer        "Arthur Pai"
 maintainer_email  "baidragoon@gmail.com"
 description       "Monit configs for server components"
-version           "0.1.2"
+version           "0.1.3"
 
 recipe "monit_configs-tlq::postgres", "Monit config for postgres"
 recipe "monit_configs-tlq::nginx", "Monit config for nginx"
